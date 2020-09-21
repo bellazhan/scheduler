07e015b3914182827789cecd22842e527f6bed24
Bella Zhan <bellazhan2021@u.northwestern.edu>
2020-09-21 14:17:08 -0500

    initial app

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const schedule = {
+  title: "CS Courses for 2018-2019"
+};
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}>Welcome to my app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.bannerStyle}>{schedule.title}</Text>
     </View>
   );
 }
@@ -14,12 +16,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00f',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  textStyle: {
-    color: '#fff',
+  bannerStyle: {
+    color: '#888',
     fontSize: 32,
   }
 });
+
+export default App;
